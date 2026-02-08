d4f19f453ef49f4c4706f7629d4c54d9a1dfd6de
riaanadvani1-svg <riaanadvani1@gmail.com>
2026-02-08 11:40:31 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,41 @@
-let cart=JSON.parse(localStorage.getItem("cart")||"[]");
-const prices={"PFP Pack":5,"Wallpaper":3,"Creator Bundle":10};
+// main.js
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+const prices = {
+  "PFP Pack": 5,
+  "Wallpaper": 3,
+  "Creator Bundle": 10
+};
 
-/* CART COUNT */
-if(document.getElementById("count")) document.getElementById("count").innerText=cart.length;
-
-/* ADD TO CART */
-function add(item){
-cart.push(item);
-localStorage.setItem("cart",JSON.stringify(cart));
-document.getElementById("count").innerText=cart.length;
-alert(item+" added");
+// Add item to cart
+function add(item) {
+  cart.push(item);
+  localStorage.setItem("cart", JSON.stringify(cart));
+  updateCount();
+  alert(item + " added to cart!");
 }
 
-/* GO TO CHECKOUT */
-function checkout(){
-location.href="checkout.html";
+// Update cart count in nav
+function updateCount() {
+  if(document.getElementById("count")){
+    document.getElementById("count").innerText = cart.length;
+  }
 }
+
+// Go to checkout page
+function checkout() {
+  window.location.href = "checkout.html";
+}
+
+// Populate checkout page
+window.addEventListener("load", () => {
+  updateCount();
+  if(document.getElementById("items")){
+    let total = 0;
+    const itemsHTML = cart.map(i => {
+      total += prices[i];
+      return `<tr><td>${i}</td><td>$${prices[i]}</td></tr>`;
+    }).join("");
+    document.getElementById("items").innerHTML = itemsHTML;
+    document.getElementById("total").innerText = total;
+  }
+});
